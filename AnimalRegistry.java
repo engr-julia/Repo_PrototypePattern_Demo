c7318e8491ed0e756414ab51eb209d3d2f681542
Julia Rodrigo <engr.julia.rt@gmail.com>
2026-04-07 00:55:00 +0000

    completed

---
+++ AnimalRegistry.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class AnimalRegistry {
+    private Map<String, Animal> prototypes = new HashMap<>();
+
+    public AnimalRegistry() {
+        // Initialize the master copies
+        prototypes.put("sheep", new Sheep());
+        prototypes.put("cow", new Cow());
+        prototypes.put("horse", new Horse());
+    }
+
+    public Animal createSheep() {
+        return prototypes.get("sheep").clone();
+    }
+
+    public Animal createCow() {
+        return prototypes.get("cow").clone();
+    }
+
+    public Animal createHorse() {
+        return prototypes.get("horse").clone();
+    }
+}
